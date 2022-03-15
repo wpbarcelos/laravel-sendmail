2fb1e2465310b5abf1991d7202a96d6524bd2b07
Wanderson Passos Barcelos <wp.barcelos@gmail.com>
2022-03-15 16:09:21 -0300

    Adicionado rota de privacidade

---
+++ resources/views/privacidade.blade.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Adv Siqueira Policitica de privacidade</title>
+
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <title>Políticas de Privacidade</title>
+</head>
+
+<body class="bg-gray-300">
+    <div class='container mx-auto mb-24 w-5/6 leading-10 text-xl'>
+        <h1 class=' sm:text-4xl text-3xl my-8 font-bold text-center'>Política de Privacidade</h1>
+
+        <p>A sua privacidade é importante para nós. É política do site Siqueira Soluções Jurídicas 
+respeitar a sua
+            privacidade em relação a qualquer informação sua que possamos coletar no site <a
+                href="https://siqueirasolucoesjuridicas.adv.br" target="_blank" rel="noopener">Siqueira 
+Soluções
+                Jurídicas</a>, e outros sites que possuímos e operamos.</p>
+
+        <p>Trabalhamos com base na Lei de Proteção de Dados (13.709/2018) que traz garantias de privacidade,
+            confidencialidade, retenção, proteção aos direitos fundamentais de liberdade e o livre 
+desenvolvimento da
+            personalidade da pessoa. Além disso, respeitamos a Constituição Federal da República Federativa 
+do Brasil, o
+            Código de Defesa do Consumidor (Lei 8.078/90), Marco Civil da Internet (Lei 12.965/14).</p>
+        <p>
+
+        </p>
+        <p>Solicitamos informações pessoais apenas quando realmente precisamos delas para lhe fornecer um 
+serviço.
+            Fazemo-lo por meios justos e legais, com o seu conhecimento e consentimento. Também informamos por 
+que
+            estamos coletando e como será usado.</p>
+
+        <p>Deixamos claro o motivo que estamos coletando e como será usado, pois tudo é feito para correta 
+administração
+            geral.</p>
+
+        <p>Apenas retemos as informações coletadas pelo tempo necessário para fornecer o serviço solicitado. 
+Quando
+            armazenamos dados, protegemos dentro de meios comercialmente aceitáveis pela legislação atual 
+​​para evitar
+            perdas e roubos, bem como acesso, divulgação, cópia, uso ou modificação não autorizados.</p>
+
+        <p>Não compartilhamos informações de identificação pessoal publicamente ou com terceiros, exceto 
+por
+            determinação judicial.</p>
+
+        <p>O nosso site pode ter links para sites externos que não são operados por nós. Diante disto, não 
+nos
+            responsabilizamos por danos de terceiros. Esteja ciente de que não temos controle sobre o conteúdo 
+e
+            práticas de sites de terceiros e não podemos aceitar responsabilidade por suas respectivas 
+políticas de
+            privacidade.</p>
+
+        <p>Na qualidade de consumidor, você é livre para recusar a nossa solicitação de informações 
+pessoais, entendendo
+            que talvez não possamos fornecer alguns dos serviços desejados.</p>
+
+        <p>O uso continuado de nosso site será considerado como aceitação de nossas práticas em torno de 
+privacidade e
+            informações pessoais. Se você tiver alguma dúvida sobre como lidamos com dados do usuário e 
+informações
+            pessoais, entre em contato conosco.</p>
+
+        <p>Nossa política é atualizada de forma constante.</p>
+
+        <p>Fica, desde já, o titular de dados ciente que o conteúdo desta Política de Privacidade pode ser 
+alterado a
+            critério do site Siqueira Soluções Jurídicas, independente de aviso ou notificação. Em caso de 
+alteração, as
+            modificações produzem todos os efeitos a partir do momento da disponibilização no site. </p>
+
+        <p>O site Siqueira Soluções Jurídicas não se responsabiliza caso você venha utilizar seus dados de 
+forma
+            incorreta ou inverídica, ficando excluído de qualquer responsabilidade neste sentido.</p>
+
+        <h3  class='text-2xl mt-8 font-bold text-gray-600'>Compromisso do Usuário</h3>
+
+        <p>O usuário se compromete a fazer uso adequado dos conteúdos e da informação que o site Siqueira 
+Soluções
+            Jurídicas oferece e com caráter enunciativo, mas não limitativo:</p>
+
+        <ul>
+            <li>A) Não se envolver em atividades que sejam ilegais ou contrárias à boa fé e à ordem 
+pública;</li>
+            <li>B) Respeito a todas as legislações nacionais ou internacionais em que o Brasil é 
+signatário;</li>
+            <li>C) Não difundir propaganda ou conteúdo de natureza racista, xenofóbica, casas de apostas, 
+jogos de sorte
+                e azar, qualquer tipo de pornografia ilegal, de apologia ao terrorismo ou contra os direitos 
+humanos;
+            </li>
+            <li>D) Não causar danos aos sistemas físicos (hardwares) e lógicos (softwares) do site Siqueira 
+Soluções
+                Jurídicas, de seus fornecedores ou terceiros, para introduzir ou disseminar vírus 
+informáticos ou
+                quaisquer outros sistemas de hardware ou software que sejam capazes de causar danos 
+anteriormente
+                mencionados;</li>
+            <li>E) Os conteúdos publicados, possuem direitos autorais e de propriedade intelectual reservados, 
+conforme
+                estabelece a Lei de Direitos Autorais n. 9.610, de 19.2.1998 do Governo Federal Brasileiro e 
+correlatas.
+                Qualquer infringência, serão comunicados às autoridades competentes.</li>
+        </ul>
+
+        <h2 class='text-2xl mt-8 font-bold text-gray-600'>Direitos do titular de dados</h2>
+        <p>O titular de dados pessoais possui o direito de solicitar do site Siqueira Soluções Jurídicas, 
+através do
+            canal específico de tratamento, a qualquer momento, mediante requisição formal, informações 
+referentes aos
+            seus dados.</p>
+        <p>Os pedidos serão analisados conforme previsto em legislação vigente dentro de um prazo de 72 
+horas, salvo
+            determinação legal e/ou objeto de lei.</p>
+
+        <p>Os titulares de dados, segundo o texto da LGPD, podem exercer os seus direitos por meio de:</p>
+        <ul>
+            <li>Confirmação da existência de tratamento;</li>
+
+
+            <li>Anonimização, bloqueio ou eliminação de dados desnecessários, excessivos ou tratados em 
+desconformidade
+                com o disposto nesta Lei;</li>
+
+
+            <li>Informação das entidades públicas e privadas com as quais o controlador realizou uso 
+compartilhado de
+                dados;</li>
+
+            <li>Informação sobre a possibilidade de não fornecer consentimento e sobre as consequências da 
+negativa;
+            </li>
+
+        </ul>
+        <h2  class='text-2xl mt-8 font-bold text-gray-600'>Como exercer os seus direitos de titular de dados?</h2>
+        <ul>
+
+
+            <li>Para as demais solicitações em relação aos direitos do titular de dados pessoais, entre em 
+contato
+                conosco;</li>
+        </ul>
+
+        <h2  class='text-2xl mt-8 font-bold text-gray-600'>Mais informações</h2>
+        <p>Esperamos que esteja esclarecido e, como mencionado anteriormente, se houver algo que você não tem 
+certeza se
+            precisa ou não, geralmente é mais seguro deixar os cookies ativados, caso interaja com um dos 
+recursos que
+            você usa em nosso site.</p>
+
+        <p>O site Siqueira Soluções Jurídicas empregará esforços para resguardar as informações e dados 
+coletados do
+            usuário pelo site. Todavia, considerando que não há meio de transmissão e retenção de dados 
+eletrônicos
+            plenamente eficaz e seguro, o site Siqueira Soluções Jurídicas não pode assegurar que terceiros 
+não
+            autorizados não logrem êxito no acesso indevido, eximindo-se de qualquer responsabilidade por 
+danos e
+            prejuízos decorrentes da conduta de terceiros, ataques externos ao site como: vírus, invasão ao 
+banco de
+            dados, vícios ou defeitos técnicos, assim como operacionais resultante da utilização do site e 
+em razão de
+            falhas de conexão.</p>
+
+
+    </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,6 @@ Route::view('/','welcome');
 Route::get('/sendmail', [SendMailController::class, 'create'])->name('sendmail.create');
 
 Route::post('/sendmail', [SendMailController::class, 'store'])->name('sendmail.store');
+
+
+Route::view('privacidade', 'privacidade');
